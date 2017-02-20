79559c59465aaef87671014a70535e43f33dc2fc
meamuri <meammf@yandex.ru>
2017-02-20 17:32:14 +0300

    we get all films as list

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'rest-client'
+require 'json'
+
+BASE_URL = 'http://swapi.co/api/'
+
+json = RestClient.get BASE_URL + 'films/'
+films = JSON.parse json
+
+films['results'].each do |f|
+  film = Film.new
+  film.director = f['director']
+  film.episode_id = f['episode_id']
+  film.opening_crawl = f['opening_crawl']
+  film.producer = f['producer']
+  film.release_date = f['release_date']
+  film.title = f['title']
+  film.save
+end
