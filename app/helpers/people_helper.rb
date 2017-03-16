3ca4f64b12bbc9f05c09e9d4067397959661de6a
meamuri <meammf@yandex.ru>
2017-03-16 18:38:01 +0300

    helper method for people birth year calculating was added

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -1,2 +1,12 @@
 module PeopleHelper
+  def show_format_birth_year(birth_year)
+    return 'unknown' if birth_year.blank?
+    if birth_year < 0
+      birth_year = -1 * (birth_year + 1)
+      birth_year.to_s + 'BBY'
+    else
+      birth_year.to_s + 'ABY'
+    end
+
+  end
 end
